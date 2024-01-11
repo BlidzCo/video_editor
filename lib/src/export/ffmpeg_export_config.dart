75d52dd36e175b8eab93b7cc7ea96f08d7aeec3b
Azamat <azamat@blidz.com>
2024-01-11 20:00:28 +0500

    Fix minor

--- lib/src/export/ffmpeg_export_config.dart
+++ lib/src/export/ffmpeg_export_config.dart
@@ -87,7 +87,7 @@ abstract class FFmpegVideoEditorConfig {
     double videoWidth = controller.video.value.size.height;
     double videoHeight = controller.video.value.size.width;
 
-    return "-i $overlayPath -filter_complex '[1:v] scale=$videoWidth:$videoHeight [overlay]; [0:v][overlay] overlay=0:0 [out]' -map '[out]'";
+    return "-i $overlayPath -filter_complex 'overlay=50:main_h-overlay_h-50'";
   }
 
   /// Returns the list of all the active filters
